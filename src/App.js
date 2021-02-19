c1dc34a0bff2cda1b1fe5f5a4f1957bb0a8365fd
Mes Morts <titouanescorneboueu@gmail.com>
2021-02-19 22:12:04 +0100

    Creation de ma navBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './scss/style.scss';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import NavBar from './components/NavBar';
+
+
+// const routes = [
+//   {path}
+// ]
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <main>
+        <NavBar/>
+        <Switch>
+          <div className="App">
+            
+          </div>
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <img src="https://images.pexels.com/photos/6032870/pexels-photo-6032870.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Un avion"/>
+            <div>
+                <p>Projets</p>
+                <p>Contact</p>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
