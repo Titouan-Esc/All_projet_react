587f57b151424d13dd45f0ee745eec630ad2d8a4
Mes Morts <titouanescorneboueu@gmail.com>
2021-02-20 22:33:41 +0100

    Fin de la navBar et de son style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import NavAndButton from './components/NavAndButton';
+import scss from './scss/style.scss';
+
+
+
 function App() {
   return (
     <main>
+      <Router>
+        <NavAndButton/>
+        <Switch>
 
+        </Switch>
+      </Router>
     </main>
   );
 }
--- src/components/NavAndButton.jsx
+++ src/components/NavAndButton.jsx
@@ -1,8 +1,18 @@
 const NavAndButton = () => {
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <h1>SnapShot</h1>
+            <div className="button_and_recherche">
+                <input type="text"/>
+                <button>Serch</button>
+            </div>
+            <div className="choises">
+                <button>Bouffe</button>
+                <button>Montagnes</button>
+                <button>Oiseaux</button>
+                <button>Plage</button>
+            </div>
+        </nav>
     )
 }
 
