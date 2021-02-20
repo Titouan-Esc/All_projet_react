f89556881a1f386fe193910bcfdbdf6463f5d006
Mes Morts <titouanescorneboueu@gmail.com>
2021-02-20 21:07:58 +0100

    Supression de tout pour tout refaire en mieux!

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import './scss/style.scss';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import NavBar from './components/NavBar';
-import Home from "./pages/Home";
-import Contact from "./pages/Contact";
-import Projets from "./pages/Projets"
-
 function App() {
   return (
-    <Router>
-      <main>
-        <NavBar/>
-        <Switch>
-          <Route path="/" exact component = {Home} />
-          <Route path="/Contact" component = {Contact} />
-          <Route path = "/Projets" component = {Projets} />
-        </Switch>
-      </main>
-    </Router>
+    <main>
+
+    </main>
   );
 }
 
--- src/pages/Projets.js
+++
@@ -1,9 +0,0 @@
-const Projets = () => {
-    return (
-        <div>
-            <h2>Projets</h2>
-        </div>
-    )
-}
-
-export default Projets
